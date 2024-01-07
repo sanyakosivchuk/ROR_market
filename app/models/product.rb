8a15d132b0cd17b8100f7f4c7df3507edeba5e2c
bodyyyya <vbogdanua177@gmail.com>
2024-01-07 17:03:31 +0200

    updated_uploading_images

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,5 @@ class Product < ApplicationRecord
     validates :name, presence:true
     validates :description, presence:true
     validates :price, presence:true
-    has_one_attached :image
+    has_many_attached :images
 end
